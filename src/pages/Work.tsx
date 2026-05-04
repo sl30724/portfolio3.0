bc3490330bc8bd0f0b278a4aa372f5bebb4f1d82
Sandy Lee <sl30724@gmail.com>
2026-05-03 22:20:08 -0600

    updates HERO intro

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -8,8 +8,8 @@ export default function Work() {
             </div>
             <div className='content'>
                 <div className='headline'>
-                    <h1 className='gradientText'>Making complex products feel simple.</h1>
-                    <p>Hi, I'm Sandy, a Product Designer who’s led end-to-end design across 6+ platforms—blending UX, research, and product thinking to turn messy, data-heavy problems into tools people actually understand and use.
+                    <h1 className='gradientText'>Sandy makes complex products feel simple.</h1>
+                    <p>I'm a Product Designer who’s led end-to-end design across 6+ platforms—blending UX, research, and product thinking to turn messy, data-heavy problems into tools people actually understand and use.
                     </p>
                     <div className='buttons'>
                         <Link className='primaryBut small' role="button" to="/about">Learn About Me</Link>
